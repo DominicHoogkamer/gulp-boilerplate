2f0fe13937f838fe57ae4907ddf6e3ef0f9ec981
Dominic Hoogkamer <dominichoogkamer@gmail.com>
2017-02-15 19:07:42 +0100

    first push of gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+"use strict";
+
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var open = require('gulp-open');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
+var lint = require('gulp-eslint');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+
+var config = {
+    port: 8080,
+    devBaseUrl: 'http://localhost',
+    paths: {
+        html: './src/*.html',
+        js: './src/**/*.js',
+        css: [
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+      		'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+        ],
+        dist: './dist',
+        mainJs: './src/main.js'
+    }
+}
+
+//Start a local development server
+gulp.task('connect', function () {
+    connect.server({
+        root: ['dist'],
+        port: config.port,
+        base: config.devBaseUrl,
+        livereload: true
+    })
+});
+
+gulp.task('open', ['connect'], function(){
+    gulp.src('dist/index.html')
+        .pipe(open({ 
+            uri: config.devBaseUrl + ':' + config.port + '/',
+            app: 'chrome'
+        }));
+});
+
+gulp.task('html', function(){
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist))
+        .pipe(connect.reload());
+});
+
+gulp.task('js', function() {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
+});
+
+gulp.task('css', function() {
+	gulp.src(config.paths.css)
+        .pipe(concat('bundle.css'))
+		.pipe(gulp.dest(config.paths.dist + '/css'));
+});
+
+gulp.task('lint', function() {
+	return gulp.src(config.paths.js)
+		.pipe(lint({configFile: 'eslint.config.json'}))
+		.pipe(lint.format());
+});
+
+gulp.task('watch', function(){
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js', 'lint']);
+    gulp.watch(config.paths.css, ['css']);
+});
+
+gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
+
---
+++ src/main.js
@@ -0,0 +1,7 @@
+$ = jQuery = require('jquery');
+
+var App = console.log('Hello world from Browserify');
+
+test = 1;
+
+module.exports = App;
